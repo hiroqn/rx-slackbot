0b51d94b92ec26693570a7357e571da750cddc2c
hiroqn <hiroqn1008@gmail.com>
2015-11-08 02:52:20 +0900

    refactor(package.json): update node and babel version

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
-import del from 'del';
-import gulp from 'gulp';
-import babel from 'gulp-babel';
-import eslint from 'gulp-eslint';
-import jscs from 'gulp-jscs';
-import runSequence from 'run-sequence';
+const gulp = require('gulp');
+const del = require('del');
+const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
+const jscs = require('gulp-jscs');
+const runSequence = require('run-sequence');
 
 gulp.task('eslint', () =>
-  gulp.src(['gulpfile.babel.js', 'src/*.js'])
+  gulp.src(['gulpfile.js', 'src/*.js'])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failOnError()));
 
 gulp.task('jscs', () =>
-  gulp.src(['gulpfile.babel.js', 'src/*.js'])
+  gulp.src(['gulpfile.js', 'src/*.js'])
     .pipe(jscs({ esnext: true })));
 
 gulp.task('clean', cb => del(['lib'], { dot: true }, cb));
@@ -23,7 +23,7 @@ gulp.task('babel', () =>
     .pipe(gulp.dest('lib')));
 
 gulp.task('watch', () => {
-  gulp.watch(['gulpfile.babel.js', 'src/*.js'], ['jscs', 'eslint']);
+  gulp.watch(['gulpfile.js', 'src/*.js'], ['jscs', 'eslint']);
 });
 
 gulp.task('test', ['eslint', 'jscs']);
