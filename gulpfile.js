8ba3ebfe7063d0677a3ac10c79a165d01e741a74
hiroqn <hiroqn1008@gmail.com>
2016-03-06 04:22:00 +0900

    chore(*): rewrite gulp

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const sourcemaps = require('gulp-sourcemaps');
+const xo = require('gulp-xo');
+const plumber = require('gulp-plumber');
+const del = require('del');
+
+gulp.task('build', gulp.series(
+  () => del('lib'),
+  () =>
+    gulp.src('src/**/*.js')
+      .pipe(sourcemaps.init())
+      .pipe(babel({
+        plugins: [
+          'transform-es2015-modules-commonjs',
+          'transform-es2015-destructuring',
+          'transform-es2015-parameters'
+        ]
+      }))
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest('lib'))));
+
+const lint = () =>
+  gulp.src(['src/**/*.js', 'gulpfile.js'])
+    .pipe(plumber())
+    .pipe(xo());
+
+gulp.task('watch', gulp.series(lint, () => {
+  gulp.watch(['src/**/*.js', 'gulpfile.js'], lint);
+}));
